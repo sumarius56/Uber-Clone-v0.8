89cb74896af240f37dde45a8718a5a45d9ac30a3
sumarius56 <110302280+sumarius56@users.noreply.github.com>
2022-08-27 23:30:58 +0300

    fixed bugs

--- pages/Components/RideSelector.js
+++ pages/Components/RideSelector.js
@@ -1,6 +1,5 @@
 import tw from "tailwind-styled-components"
 import React from "react"
-import { carList } from "../data/carList"
 import { useEffect, useState } from "react"
 
 
@@ -9,6 +8,34 @@ import { useEffect, useState } from "react"
 
 const RideSelector = ({pickupCoordinates, dropoffCoordinates}) => {
    
+
+  const carList = [
+  {
+    imgUrl: 'https://i.ibb.co/cyvcpfF/uberx.png',
+    service: 'UberX',
+    multiplier: 1,
+  },
+  {
+    imgUrl: 'https://i.ibb.co/YDYMKny/uberxl.png',
+    service: 'UberXL',
+    multiplier: 1.5,
+  },
+  {
+    imgUrl: 'https://i.ibb.co/Xx4G91m/uberblack.png',
+    service: 'Black',
+    multiplier: 2,
+  },
+  {
+    imgUrl: 'https://i.ibb.co/cyvcpfF/uberx.png',
+    service: 'Comfort',
+    multiplier: 1.2,
+  },
+  {
+    imgUrl: ' https://i.ibb.co/1nStPWT/uberblacksuv.png',
+    service: 'Black SUV',
+    multiplier: 2.8,
+  }
+]
   const [rideDuration,setRideDuration] = useState(0)
 
   useEffect(()=>{
